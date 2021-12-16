cef4c8ebb28f8cd85a2151843b0341f07cf73a6e
Goran Milic <gmilic@gmail.com>
2021-12-16 16:58:24 +0100

    aarp home widget new modals

---
+++ local-test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script
+      type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmXPM19Ep9PXLuA9f7-XbVJEwMMrVsWqQ&libraries=places"
+    ></script>
+    <script src="https://unpkg.com/v-calendar"></script>
+  </head>
+  <body>
+    <div id="includedContent"></div>
+    <?php include "./home-widget-aarp.html" ?>
+  </body>
+</html>
