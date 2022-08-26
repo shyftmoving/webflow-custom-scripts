81c5c98e82bbf1f04aee9a10ddc8f9b2a0fb7477
Goran Milic <gmilic@gmail.com>
2022-08-26 13:22:06 +0200

    cs - modal as separate page

--- local-test.php
+++ local-test.php
@@ -6,15 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
 
-    <script
-      src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-    
 
-  <script src="https://unpkg.com/v-calendar"></script>
 
   <style>
     body  {
@@ -25,11 +17,10 @@
   </head>
   <body>
     <div id="includedContent"></div>
-    <script
-  type="text/javascript"
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmXPM19Ep9PXLuA9f7-XbVJEwMMrVsWqQ&libraries=places"
-></script>
-<script src="https://unpkg.com/v-calendar"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmXPM19Ep9PXLuA9f7-XbVJEwMMrVsWqQ&libraries=places"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="https://unpkg.com/v-calendar"></script>
+
     <?php include "./home-widget-cityswitcher.html" ?>
   </body>
 </html>
